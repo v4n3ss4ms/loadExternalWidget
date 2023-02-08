300680651966707324496ee85727036ee15e74b9
v4n3ss4ms <v4n3ss4ms@gmail.com>
2023-02-08 20:16:47 +0100

    [TESTS] remove comment

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    //baseUrl: 'http://127.0.0.1:4000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
